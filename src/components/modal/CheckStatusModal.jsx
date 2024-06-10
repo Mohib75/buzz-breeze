60f7ef93413498b21e1443f429fd9b014eabf4e3
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-06-10 22:57:11 +0600

    modal added

---
+++ src/components/modal/CheckStatusModal.jsx
@@ -0,0 +1,61 @@
+import { Dialog, Transition, TransitionChild, DialogPanel, DialogTitle } from "@headlessui/react"
+import { Fragment } from "react"
+import { Link } from "react-router-dom"
+
+const CheckStatusModal = ({ isOpen, closeModal }) => {
+	return (
+		<Transition appear show={isOpen} as={Fragment}>
+			<Dialog as='div' className='relative z-10' onClose={closeModal}>
+				<TransitionChild
+					as={Fragment}
+					enter='ease-out duration-300'
+					enterFrom='opacity-0'
+					enterTo='opacity-100'
+					leave='ease-in duration-200'
+					leaveFrom='opacity-100'
+					leaveTo='opacity-0'>
+					<div className='fixed inset-0 bg-black bg-opacity-25' />
+				</TransitionChild>
+
+				<div className='fixed inset-0 overflow-y-auto'>
+					<div className='flex min-h-full items-center justify-center p-4 text-center'>
+						<TransitionChild
+							as={Fragment}
+							enter='ease-out duration-300'
+							enterFrom='opacity-0 scale-95'
+							enterTo='opacity-100 scale-100'
+							leave='ease-in duration-200'
+							leaveFrom='opacity-100 scale-100'
+							leaveTo='opacity-0 scale-95'>
+							<DialogPanel className='w-full max-w-md transform overflow-hidden rounded-2xl !bg-[#313046] p-6 text-left align-middle shadow-xl transition-all'>
+								<DialogTitle as='h3' className='text-lg font-medium leading-6 text-[#b9b9c8]'>
+									You have limit your max limit of your post
+								</DialogTitle>
+								<div className='mt-2'>
+									<p className='text-sm text-gray-500'>If you want to add more post, subscribe to our membership page</p>
+								</div>
+								<hr className='mt-8 ' />
+								<div className='flex mt-2 justify-around'>
+									<Link
+										to='/membership'
+										type='button'
+										className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'>
+										Go To Membership
+									</Link>
+									<button
+										type='button'
+										className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
+										onClick={closeModal}>
+										No
+									</button>
+								</div>
+							</DialogPanel>
+						</TransitionChild>
+					</div>
+				</div>
+			</Dialog>
+		</Transition>
+	)
+}
+
+export default CheckStatusModal
