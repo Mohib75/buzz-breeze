cfa4f58884015c5f49d35cec98ebdf502fdfc7d8
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-06-10 22:57:55 +0600

    announcement added

---
+++ src/components/home/AnnouncementCard.jsx
@@ -0,0 +1,38 @@
+import { TfiAnnouncement } from "react-icons/tfi"
+import { Link } from "react-router-dom"
+
+const AnnouncementCard = ({ announcement }) => {
+	return (
+		<Link to={`/post/${announcement?._id}`} className='hover:bg-[#222038] transition-all duration-500 cursor-pointer p-4 rounded-2xl'>
+			<div className='flex justify-between items-center mt-4 gap-20'>
+				<div className='flex gap-4 w-80'>
+					<TfiAnnouncement className='text-[#2575ED]' size={26} />
+
+					<div className='text-xl text-white w-60'>Announcement</div>
+				</div>
+
+				<div className='flex flex-col gap-4 items-center w-[476px]'>
+					<p className='text-[#b9b9c8]'>Description</p>
+					<p className='text-white w-[476px]'>{announcement?.description}</p>
+				</div>
+
+				<div className='flex flex-col gap-4 items-center'>
+					<p className='text-[#b9b9c8]'>Title</p>
+					<p className='text-white'>{announcement?.title}</p>
+				</div>
+
+				<div className='flex gap-4 items-center'>
+					<div className='avatar'>
+						<div className='w-16 rounded-full ring ring-[#b9b9c8] ring-offset-[#b9b9c8] ring-offset-2'>
+							<img src={announcement?.image} />
+						</div>
+					</div>
+
+					<p className='text-white mr-2'>By author</p>
+				</div>
+			</div>
+		</Link>
+	)
+}
+
+export default AnnouncementCard
