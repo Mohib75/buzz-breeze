2de14a93e8a1a11a7bb235dc43f08e1671cf31ee
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-06-10 22:59:14 +0600

    tag added

---
+++ src/components/tags/TagBox.jsx
@@ -0,0 +1,36 @@
+import queryString from "query-string"
+import { FaComment } from "react-icons/fa"
+import { useNavigate, useSearchParams } from "react-router-dom"
+
+const TagBox = ({ tags }) => {
+	// eslint-disable-next-line no-unused-vars
+	const [params, setParams] = useSearchParams()
+	const selectedTags = params.get("tags")
+
+	const navigate = useNavigate()
+	const handleClick = () => {
+		// 1. Create Query String
+		let currentQuery = { tags: tags }
+		const url = queryString.stringifyUrl({
+			url: "/",
+			query: currentQuery,
+		})
+		// url output---> /?tags=tags
+
+		// 2. Set query string in url
+		navigate(url)
+	}
+
+	return (
+		<div
+			onClick={handleClick}
+			className={`flex flex-col items-center justify-center gap-2 p-3 border-r-2 bg-[#313046] text-white hover:text-[#b9b9cb] transition cursor-pointer  ${
+				selectedTags === tags && "text-[#a9a9c7] border-b-2 border-white "
+			} `}>
+			<FaComment className='text-[#2575ED]' size={26} />
+			<div className='text-sm font-medium'>{tags}</div>
+		</div>
+	)
+}
+
+export default TagBox
---
+++ src/components/tags/Tags.jsx
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query"
+import useAxiosCommon from "../../hooks/useAxiosCommon"
+import TagBox from "./TagBox"
+
+const Tags = () => {
+	const axiosCommon = useAxiosCommon()
+	// const [loading, setLoading] = useState(false)
+
+	const { data: tags = [] } = useQuery({
+		queryKey: ["tags"],
+
+		queryFn: async () => {
+			const { data } = await axiosCommon.get(`/tags`)
+			return data
+		},
+	})
+
+	return (
+		<div className='flex flex-col gap-8 bg-[#313046] overflow-x-auto rounded-2xl mb-12 py-4 mx-4 sm:mx-0'>
+			<h2 className='text-4xl text-white font-bold self-center'>Tags</h2>
+			<div className='pt-4 flex items-center justify-between'>
+				{tags.map((tag, index) => (
+					<TagBox key={index} tags={tag.tags} />
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default Tags
