368d8a7049e6a5bc0638ebade2d4e6d72278a5c4
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-09-15 23:35:05 +0600

    announcement updated

--- src/components/home/Announcement.jsx
+++ src/components/home/Announcement.jsx
@@ -2,7 +2,7 @@ import AnnouncementCard from "./AnnouncementCard"
 
 const Announcement = ({ announcements }) => {
 	return (
-		<div className='flex flex-col bg-[#313046] rounded-2xl overflow-x-auto p-10 mb-12 mx-4 sm:mx-0'>
+		<div className='flex flex-col bg-[#313046] rounded-2xl overflow-x-auto p-10 mb-12'>
 			<h2 className='text-2xl sm:text-4xl text-white font-bold self-center'>Announcements</h2>
 			<div className='w-full flex flex-col gap-4 mt-4'>
 				{announcements.map((announcement, index) => (
