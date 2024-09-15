f076efbc4d19ef8ca795ff1b868986eb8b8a86b6
Ifthakher Rahman Mohib <ifthakherrahman426@gmail.com>
2024-09-15 23:35:51 +0600

    navbar updated

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -177,7 +177,7 @@ const Navbar = () => {
 								d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
 							/>
 						</svg>
-						<span className='badge badge-md badge-primary indicator-item bg-[#313046] text-[#b9b9c8]'>8</span>
+						<span className='badge badge-md badge-primary indicator-item bg-[#313046] text-[#b9b9c8]'>{[announcementsCount]}</span>
 					</div>
 
 					<Link to='/register'>
